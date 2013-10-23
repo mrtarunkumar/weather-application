3f6ff556108294c042dfd2939b0f56abe2a8df9c
mrtarunkumar <tarunkumar.vegamwala@ntlworld.com>
2013-10-23 17:47:46 +0100

    Weather Underground Logo

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Weather Application</title>
     </head>
     <body>
+        <img src="img/wundergroundLogo_black_horz.jpg" alt="Weather Underground logo" />
+        <h1>Weather Underground Application</h1>
+        
+        <!-- Weather Underground Code -->     
         <?php
         // put your code here
         ?>
