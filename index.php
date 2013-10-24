40c3f74b47f52e9297c45429a606fc04cfd34116
mrtarunkumar <tarunkumar.vegamwala@ntlworld.com>
2013-10-24 12:17:26 +0100

    New index page with form

--- index.php
+++ index.php
@@ -2,31 +2,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Weather Application</title>
+        <title></title>
     </head>
     <body>
         <img src="img/wundergroundLogo_black_horz.jpg" alt="Weather Underground logo" />
         <h1>Weather Underground Application</h1>
-        
-        <!-- Weather Underground Code -->     
-        <?php
-        // Weather Underground API request
-        // Store initial request
-        $json_string = file_get_contents("http://api.wunderground.com/api/c351cf41056050c6/geolookup/conditions/q/UK/Leicester.json");
-        
-        // Decodes JSON string
-       $decoded_string = json_decode($json_string,true);
-        
-        //Print 
-        //print_r($decoded_string);
-        
-       
-        $location_country = $decoded_string['current_observation']['display_location']['country']; 
-        $location_city = $decoded_string['current_observation']['display_location']['city'];
-        
-        echo "Country: " . $location_country . "<br />";
-        echo "City: " . $location_city . "<br/>";
-
-        ?>
+        <p>Please enter a UK city name</p>
+        <form action="process.php" method="post">
+            City: <input type="text" name="city" /><input type="submit"/>
+        </form>
     </body>
 </html>
